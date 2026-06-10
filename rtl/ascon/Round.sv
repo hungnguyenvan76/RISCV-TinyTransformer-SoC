d53c6054d91836135e9581d63c705bd1018032ad
HungCE2409 <nguyenhung84774821602@gmail.com>
2026-06-10 08:39:15 +0700

    edit comment module round

--- rtl/ascon/Round.sv
+++ rtl/ascon/Round.sv
@@ -14,39 +14,37 @@ module Round
     output logic [0:4][63:0] x_out,
     input  logic [7:0]       round_const
 );
-    // Khai báo các thanh ghi trạng thái trung gian cục bộ (64-bit)
     logic [63:0] x0, x2, x4;
     logic [63:0] t0, t1, t2, t3, t4;
     
     always_comb begin
-        // 1. ADD ROUND CONSTANT
+        // ADD ROUND CONSTANT
         x0 = x_in[0];
         // x1 = x_in[1];
         x2 = x_in[2] ^ {56'b0, round_const};
         // x3 = x_in[3];
         x4 = x_in[4];
 
-        // 2. SUBSTITUTION LAYER
-        // Bước Pre S-box
+        // SUBSTITUTION LAYER
+        // Pre S-box
         x0 = x0 ^ x4;
         x2 = x2 ^ x_in[1];
         x4 = x4 ^ x_in[3];
         
-        // Bước Keccak S-box (Lớp phi tuyến)
-        // Gán vào biến t để giữ nguyên giá trị x không bị ghi đè chéo
+        // Keccak S-box (non-linear layer)
         t0 = x0 ^ (~x_in[1] & x2);
         t1 = x_in[1] ^ (~x2 & x_in[3]);
         t2 = x2 ^ (~x_in[3] & x4);
         t3 = x_in[3] ^ (~x4 & x0);
         t4 = x4 ^ (~x0 & x_in[1]);
         
-        // Bước Post S-box
+        // Post S-box
         t1 = t1 ^ t0;
         t0 = t0 ^ t4;
         t3 = t3 ^ t2;
         t2 = ~t2;
 
-        // 3. LINEAR DIFFUSION LAYER (Xuất thẳng ra cổng ngõ ra)
+        // LINEAR DIFFUSION LAYER 
         x_out[0] = t0 ^ ROR(t0, 19) ^ ROR(t0, 28);
         x_out[1] = t1 ^ ROR(t1, 61) ^ ROR(t1, 39);
         x_out[2] = t2 ^ ROR(t2,  1) ^ ROR(t2,  6);
